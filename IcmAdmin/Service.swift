c4e617854cf0f8cea58f484d4a14126c225786ad
darthjuda <mehdi.jung@me.com>
2019-10-27 19:06:55 +0100

    Initial Commit
    Signed-off-by: darthjuda <mehdi.jung@me.com>

---
+++ IcmAdmin/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  IcmAdmin
+//
+//  Created by mehdi jung on 27/10/2019.
+//  Copyright © 2019 mehdi jung. All rights reserved.
+//
+
+import Foundation
+
+struct Service: Codable {
+    var id: Int
+    var name: String
+    var description: String
+    var price: Int
+    
+}
--- IcmAdmin/ViewController.swift
+++ IcmAdmin/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var services = [Service]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchData()
     }
 
+    func fetchData() {
+        let url = URL(string: "http://localhost:8080/services")!
+        
+        URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Unknown error")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            if let servicesArray = try? decoder.decode([Service].self, from: data) {
+                DispatchQueue.main.async {
+                    self.services = servicesArray
+                    self.tableView.reloadData()
+                    print("Loaded \(servicesArray.count) services.")
+                }
+            } else {
+                print("unable to parse JSON response.")
+            }
+        }.resume()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return services.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let service = services[indexPath.row]
+        cell.textLabel?.text = "\(service.name) - $\(service.price)"
+        cell.detailTextLabel?.text = service.description
+        
+        return cell
+    }
 }
 
